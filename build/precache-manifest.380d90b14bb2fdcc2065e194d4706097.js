b8c8cdf37d910b6c8a302bea100d97e07d4fe6be
VAbilakshan3 <abilakshan3@gmail.com>
2020-10-27 18:02:44 +0530

    make it better

---
+++ build/precache-manifest.380d90b14bb2fdcc2065e194d4706097.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be4e376ef223b720b4ee05306671f412",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b367d50884e463bf407",
+    "url": "/static/css/main.4fd67680.chunk.css"
+  },
+  {
+    "revision": "81c7f804ab8857015b10",
+    "url": "/static/js/2.59fb6fb3.chunk.js"
+  },
+  {
+    "revision": "e4f6fd5e687738d725e56603861f1923",
+    "url": "/static/js/2.59fb6fb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b367d50884e463bf407",
+    "url": "/static/js/main.2391d019.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
