53d90ea304149dfe79568162c9551f9dcbd06cb4
VAbilakshan3 <abilakshan3@gmail.com>
2020-10-30 01:43:58 +0530

    heheeeeeee

---
+++ frontend/build/precache-manifest.d8ec58aa05e8a09493cf0f84854734fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ca82cb2c09815551f0c518fe3a59c4f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c2a43a2f6ee2749b7684",
+    "url": "/static/css/main.2c046795.chunk.css"
+  },
+  {
+    "revision": "81c7f804ab8857015b10",
+    "url": "/static/js/2.59fb6fb3.chunk.js"
+  },
+  {
+    "revision": "e4f6fd5e687738d725e56603861f1923",
+    "url": "/static/js/2.59fb6fb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2a43a2f6ee2749b7684",
+    "url": "/static/js/main.2db83359.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,3 @@
-// export const API = process.env.REACT_APP_API_URL;
 export const API = process.env.REACT_APP_API_URL;
-// process.env.REACT_APP_API_URL
+
 
