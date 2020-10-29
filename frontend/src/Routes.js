4050cd3f9e4ce00865e65bbabbe1b98e8464830a
VAbilakshan3 <abilakshan3@gmail.com>
2020-10-30 02:37:47 +0530

    heheeeeeee

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import {HashRouter, Switch, Route} from 'react-router-dom';
 import Signup from './user/Signup';
 import Signin from './user/Signin';
 import Home from './core/Home';
@@ -36,7 +36,7 @@ import thanksForthePayment from './core/thanksForthePayment'
 
 const Routes = () => {
     return (
-    <BrowserRouter>
+    <HashRouter>
         <Switch>
             <Route path="/" exact  component={Home}/>
             <Route path="/signin" exact  component={Signin}/>
@@ -60,7 +60,7 @@ const Routes = () => {
             <PrivateRoute path="/thanksForthePayment" exact component={thanksForthePayment} />
 
         </Switch>
-    </BrowserRouter>
+    </HashRouter>
     );
 };
 export default Routes;
